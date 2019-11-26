7660a754bdd463248a64956f599272dae2ccd5aa
bilin.wang <wangbilin@orientsoft.cn>
2019-11-26 12:23:36 +0800

    fix purchase api

--- applications/purchase.py
+++ applications/purchase.py
@@ -3,63 +3,78 @@
 from flask import Blueprint, request
 from ext import trueReturn, mongoDBHelper
 from bson import ObjectId
+from datetime import datetime
 
 purchase = Blueprint('puchase', __name__)
 
 
 @purchase.route('/purchase', methods=['GET'])
 def get_purchase():
-    db = mongoDBHelper()
-    purchase_list = list(db.purchase.find({'delete': False}))
-    project_list = list(db.project.find({'delete': False}))
-    image_list = list(db.image.find({'delete': False}))
-    image_dict = {}
-    for image in image_list:
-        image['_id'] = str(image['_id'])
-        image_dict[image['_id']] = image
-    project_dict = {}
-    for project in project_list:
-        project_dict[project['_id']] = {
-            '_id': str(project['_id']),
-            'title': project['title'],
-            'description': project['description'],
-            'timeConsume': project['timeConsume'],
-            'createdAt': project['createdAt'],
-            'updatedAt': project['updatedAt'],
-            'labs': project['labs'],
-            'spec': project['spec'],
-            'repoName': project['repoName'],
-            'hisUrl': project.get('hisUrl'),
-            'image': image_dict[str(project['image'])]
-        }
-    for purchase in purchase_list:
-        purchase['project'] = project_dict.get(purchase['project']) if project_dict.get(purchase['project']) else {
-            '_id': None,
-            'title': None,
-            'description': None,
-            'timeConsume': None,
-            'createdAt': None,
-            'updatedAt': None,
-            'labs': None,
-            'spec': None,
-            'repoName': None,
-            'hisUrl': None,
-            'image': None
-        }
-        purchase['_id'] = str(purchase['_id'])
-        purchase['purchaser'] = str(purchase['purchaser'])
-    return trueReturn(purchase_list)
+    db = mongoDBHelper('MOOP_SERVICE')
+    # purchase_list = list(db.purchase.find({'delete': False}))
+    # project_list = list(db.project.find({'delete': False}))
+    # image_list = list(db.image.find({'delete': False}))
+    # image_dict = {}
+    # for image in image_list:
+    #     image['_id'] = str(image['_id'])
+    #     image_dict[image['_id']] = image
+    # project_dict = {}
+    # for project in project_list:
+    #     project_dict[project['_id']] = {
+    #         '_id': str(project['_id']),
+    #         'title': project['title'],
+    #         'description': project['description'],
+    #         'timeConsume': project['timeConsume'],
+    #         'createdAt': project['createdAt'],
+    #         'updatedAt': project['updatedAt'],
+    #         'labs': project['labs'],
+    #         'spec': project['spec'],
+    #         'repoName': project['repoName'],
+    #         'hisUrl': project.get('hisUrl'),
+    #         'image': image_dict[str(project['image'])]
+    #     }
+    # for purchase in purchase_list:
+    #     purchase['project'] = project_dict.get(purchase['project']) if project_dict.get(purchase['project']) else {
+    #         '_id': None,
+    #         'title': None,
+    #         'description': None,
+    #         'timeConsume': None,
+    #         'createdAt': None,
+    #         'updatedAt': None,
+    #         'labs': None,
+    #         'spec': None,
+    #         'repoName': None,
+    #         'hisUrl': None,
+    #         'image': None
+    #     }
+    #     purchase['_id'] = str(purchase['_id'])
+    #     purchase['purchaser'] = str(purchase['purchaser'])
+    result = []
+    dataList = db.purchase.aggregate([
+        {'$match': {'delete': False}},
+        {"$lookup": {'from': 'tenant', 'localField': 'purchaser', 'foreignField': '_id', 'as': 'tenant'}},
+        {"$lookup": {'from': 'project', 'localField': 'project', 'foreignField': '_id', 'as': 'project'}},
+    ])
+    for d in dataList:
+        result.append({
+            'tenantid': str(d['tenant'][0]['_id']),
+            'tenantname': d['tenant'][0]['name'],
+            'namespace': d['tenant'][0]['namespace'],
+            'limit': datetime.strftime(d['limit'], '%Y-%m-%d'),
+            'projecttitle': d['project'][0]['title'],
+            'purchaseid': str(d['_id'])
+        })
+    return trueReturn(result)
 
 
 @purchase.route('/purchase', methods=['POST'])
 def insert_purchase():
-    from datetime import datetime
-    db = mongoDBHelper()
+    db = mongoDBHelper('MOOP_SERVICE')
     purchase = request.json
     db.purchase.insert_one({
         'purchaser': ObjectId(purchase['tenantid']),
         'project': ObjectId(purchase['projectid']),
-        'limit': datetime.strptime(purchase['expire'], '%Y-%m-%d'),
+        'limit': datetime.strptime(purchase['limit'], '%Y-%m-%d'),
         'createdAt': datetime.now(),
         'updatedAt': datetime.now(),
         'remark': purchase.get('remark'),
