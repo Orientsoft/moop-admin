fa7fccf4a6abfce3c489fb31834097ba182d689b
bilin.wang <wangbilin@orientsoft.cn>
2019-11-27 13:19:38 +0800

    fix url

--- public/js/components/header.js
+++ public/js/components/header.js
@@ -6,14 +6,14 @@ define(['preact', 'util'], function (preact, util) {
           <div class="sui-container">
             <a href="/" class="sui-brand">moop-admin</a>
             ${user ? preact.html`
+              <ul class="sui-nav pull-right">
+                <li><a >${user.name}</a></li>
+                <li><a onClick=${onLogout}>退出</a></li>
+              </ul>
+            ` : preact.html`
               <form class="sui-form sui-form pull-right">
                 <a href="/" class="sui-btn btn-primary">登录</a>
               </form>
-            ` : preact.html`
-              <ul class="sui-nav pull-right">
-                <li><a href="#">用户名</a></li>
-                <li><a onClick=${onLogout}>退出</a></li>
-              </ul>
             `}
           </div>
         </div>
